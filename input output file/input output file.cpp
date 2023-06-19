eb80ee7f267128426c832e44ad833de454d102b0
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-06-19 14:59:21 +0700

    d

--- input output file/input output file.cpp
+++ input output file/input output file.cpp
@@ -31,4 +31,12 @@ int main() {
 	ifstream infile;
 	//menunjuk ke sebuah file
 	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open()) {
+		//melakukan perulangan setiap baris
+		while(getline(infile, baris))
+	}
 }
+
