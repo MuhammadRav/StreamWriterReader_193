23a1f5137387ea224cc43daa162e69a6c0dcf711
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-06-19 14:56:21 +0700

    as

--- input output file/input output file.cpp
+++ input output file/input output file.cpp
@@ -21,5 +21,8 @@ int main() {
 		//loop akan berhenti jika anda memasukkan karakter q
 		if (baris == "q")
 			break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
 	}
+
 }
