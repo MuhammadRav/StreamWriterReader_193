98b82a7f06baa20c21097ce7ba7e45367bb64f45
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-06-19 15:31:07 +0700

    ww

--- input output file/input output file.cpp
+++ input output file/input output file.cpp
@@ -45,7 +45,8 @@ int main() {
 		infile.close();
 	}
 	//jika tidak ditemukan file maka akan menampilkan ini
-	else cout << "Unable to open file";
+	else
+		cout << "Unable to open file";
 	return 0;
 }
 
