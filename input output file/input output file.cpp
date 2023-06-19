7643e594112b69b1dc4d46c63f2b61b72f8c9d28
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-06-19 14:57:34 +0700

    aq

--- input output file/input output file.cpp
+++ input output file/input output file.cpp
@@ -24,5 +24,11 @@ int main() {
 		//menulis dan memasukan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
 	}
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
 
+	//membuka dile dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");
 }
